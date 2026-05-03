0a5b044a91fcb4444f40a717fc36f249e265cd54
El-Medonho <fredericoboller@outlook.com>
2026-05-03 18:36:33 -0300

    started notebook for project

--- tensara/vectoraddtition.cu
+++ tensara/vectoraddtition.cu
@@ -1,4 +1,7 @@
 #include <cuda_runtime.h>
+#include "bits/stdc++.h"
+
+using namespace std;
 
 __global__ void solve(const float *A, const float *B, float *C, int n){
     int i = threadIdx.x + blockIdx.x * blockDim.x;
@@ -7,9 +10,38 @@ __global__ void solve(const float *A, const float *B, float *C, int n){
 }
 
 // Note: d_input1, d_input2, d_output are device pointers
-extern "C" void solution(const float* d_input1, const float* d_input2, float* d_output, size_t n) {
-    int grid = (n+255)/256, block = 256;
+// extern "C" void solution(const float* d_input1, const float* d_input2, float* d_output, size_t n) {
+//     int grid = (n+255)/256, block = 256;
 
-    solve<<<grid, block>>>(d_input1, d_input2, d_output, n);
-    cudaDeviceSynchronize();
+//     solve<<<grid, block>>>(d_input1, d_input2, d_output, n);
+//     cudaDeviceSynchronize();
+// }
+
+void execute_kernel(const float *A, const float *B, float *C, int n){
+    // the 'd' suffix indicates that this points to a device variable
+    float *Ad, *Bd, *Cd;
+
+    // this allocs VRAM memory to our variables
+    cudaMalloc((void**)Ad, n*sizeof(float));
+    cudaMalloc((void**)Bd, n*sizeof(float));
+    cudaMalloc((void**)Cd, n*sizeof(float));
+
+    // we copy the input vectors to device
+    cudaMemcpy(Ad, A, n*sizeof(float), cudaMemcpyHostToDevice);
+    cudaMemcpy(Bd, B, n*sizeof(float), cudaMemcpyHostToDevice);
+
+    // now we should determine the size of the grid and block
+    // gridDim determines the amount of blocks in the grid
+    // blockDim determines the amount of threads in a block. This number should be a multiple of 32
+
+    dim3 gridDim((n+63)/64), blockDim(64);
+
+    // execute the kernel
+    solve<<<gridDim, blockDim>>>(Ad, Bd, Cd, n);
+
+    // copy the result vector Cd to host
+
+    cudaMemcpy(C, Cd, n*sizeof(float), cudaMemcpyDeviceToHost);
+
+    return;
 }
